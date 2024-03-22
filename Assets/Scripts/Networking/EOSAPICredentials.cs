dac34e4a3af8063126e9760bb340eaf57cb80ea1
Nicolas Zordan <nicolas.zordan.tek@gmail.com>
2024-03-22 11:46:35 +0100

    EOS SDK API Credentials

---
+++ Assets/Scripts/Networking/EOSAPICredentials.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EOSAPICredentials : MonoBehaviour
+{
+    /** The product id for the running application, found on the dev portal */
+    public const string ProductId = "2543070ef04f4080aae919a7b7de449a";
+    /** The application id for the running application, found on the dev portal */
+    public const string ApplicationId = "fghi4567XKSlnQxyg6f5lX2YaoE3seml";
+    /** The sandbox id for the running application, found on the dev portal */
+    public const string SandboxId = "a21b7a5906e24d3185438b0decc424db";
+    /** The deployment id for the running application, found on the dev portal */
+    public const string DeploymentId = "b3723c58243846bd9d8c17014adc761e";
+    /** Client id of the service permissions entry, found on the dev portal */
+    public const string ClientCredentialsId = "xyza7891DgaCYPAzW46SKPUp9xEckdLp";
+    /** Client secret for accessing the set of permissions, found on the dev portal */
+    public const string ClientCredentialsSecret = "btNreqex86qx0NZLMCWGqeSmmtl8Kpif8WPXYOiK3Ao"; 
+}
